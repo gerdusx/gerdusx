4cbb2cb7d44180843eb0afed253f7fa22d7ea594
Gerdus Marais <gerdusx@gmail.com>
2024-11-01 21:44:16 +0200

    day4 - custom errors

--- Solana/RareSkills/day_04/programs/day_04/src/lib.rs
+++ Solana/RareSkills/day_04/programs/day_04/src/lib.rs
@@ -6,11 +6,29 @@ declare_id!("6VpH7jvQCXNVW1oozrSVh1EJDdC9RgZYC5D8YRrqwkcv");
 pub mod day_04 {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn limit_range(_ctx: Context<LimitRange>, a: u64) -> Result<()> {
+        require!(a >= 10, MyError::TooSmall);
+        require!(a <= 100, MyError::TooBig);
+
         Ok(())
+    }
+
+    pub fn always_errors(_ctx: Context<LimitRange>) -> Result<()> {
+        msg!("Will this print?");
+        return Err(MyError::AlwaysErrors.into());
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct LimitRange {}
+
+
+#[error_code]
+pub enum MyError {
+    #[msg("a is too small")]
+    TooSmall,
+    #[msg("a is too big")]
+    TooBig,
+    #[msg("Always errors")]
+    AlwaysErrors,
+}
--- Solana/RareSkills/day_04/tests/day_04.ts
+++ Solana/RareSkills/day_04/tests/day_04.ts
@@ -1,16 +1,47 @@
-import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
-import { Day04 } from "../target/types/day_04";
+import * as anchor from '@coral-xyz/anchor';
+import { AnchorError, Program } from '@coral-xyz/anchor';
+import { Day04 } from '../target/types/day_04';
+import { assert } from 'chai';
 
-describe("day_04", () => {
+describe('day_04', () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.Day04 as Program<Day04>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  // it('limit range', async () => {
+  //   try {
+  //     const tx = await program.methods.limitRange(new anchor.BN(5)).accounts({}).rpc();
+  //     console.log('Your transaction signature', tx);
+  //   } catch (_err) {
+  //     assert.isTrue(_err instanceof anchor.AnchorError);
+  //     const err: AnchorError = _err;
+  //     const errMsg = 'a is too small';
+  //     assert.equal(err.error.errorMessage, errMsg);
+  //     console.log('Error code', err.error.errorCode);
+  //   }
+
+  //   try {
+  //     const tx = await program.methods.limitRange(new anchor.BN(101)).accounts({}).rpc();
+  //     console.log('Your transaction signature', tx);
+  //   } catch (_err) {
+  //     assert.isTrue(_err instanceof anchor.AnchorError);
+  //     const err: AnchorError = _err;
+  //     const errMsg = 'a is too big';
+  //     assert.equal(err.error.errorMessage, errMsg);
+  //     console.log('Error code', err.error.errorCode);
+  //   }
+  // });
+
+  it('always errors', async () => {
+    try {
+      const tx = await program.methods.alwaysErrors().accounts({}).rpc();
+    } catch (_err) {
+      assert.isTrue(_err instanceof anchor.AnchorError);
+      const err: AnchorError = _err;
+      const errMsg = 'Always errors';
+      assert.equal(err.error.errorMessage, errMsg);
+      console.log('Error code', err.error.errorCode);
+    }
   });
 });
